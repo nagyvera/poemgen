855f943f014f2a4db95e4e828ec3d47be66fcebc
nagyvera <nagyvera@gamma.ttk.pte.hu>
2020-11-07 17:45:53 +0100

    Az input szó is meg van oldva, csak a text-generálás hiányzik, admin javítás

--- poemgen/admin.py
+++ poemgen/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Poem
+from .models import Poem, PoemDetails
 
 # Register your models here.
 admin.site.register(Poem)
+admin.side.register(PoemDetails)
